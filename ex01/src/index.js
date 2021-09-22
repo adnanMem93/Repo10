8d6b98880a3d75a74ea45dc8ed0464211b21bd90
adnanMem93 <adnan.mem.93@gmail.com>
2021-09-22 09:29:18 +0200

    Created folder 01

---
+++ ex01/src/index.js
@@ -0,0 +1,17 @@
+// Only change code below this line
+
+var myPet = {
+    species: "beagle",
+    name: "Toby",
+    legs: 4,
+    friends: ["Bobby", "Rex"]
+};
+
+function myFunction(myObj) {
+return myObj;
+}
+
+// Only change code above this line
+
+console.log(myFunction(myPet));  // Change this line
+module.exports = { myPet, myFunction };
